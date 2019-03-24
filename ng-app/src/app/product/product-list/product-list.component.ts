5f0cffb680cda1b314b678c409f436700c3f7a2c
Jacob <zhangen69@gmail.com>
2019-03-24 17:38:55 +0800

    refactoring code - removed useless codes

--- ng-app/src/app/product/product-list/product-list.component.ts
+++ ng-app/src/app/product/product-list/product-list.component.ts
@@ -1,31 +1,9 @@
 import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { Product } from '../../models/product.model';
-import { MatTableDataSource, MatSort, MatPaginator, PageEvent, Sort } from '@angular/material';
-import { SelectionModel } from '@angular/cdk/collections';
+import { MatTableDataSource, MatSort, MatPaginator, Sort } from '@angular/material';
 import { merge } from 'rxjs';
-import { switchMap, map } from 'rxjs/operators';
 import { StandardService } from 'src/app/services/standard.service';
 import { IQueryModel } from 'src/app/interfaces/query-model';
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' }
-];
 
 @Component({
   selector: 'app-product-list',
@@ -35,8 +13,6 @@ const ELEMENT_DATA: PeriodicElement[] = [
 export class ProductListComponent implements OnInit, AfterViewInit {
   products: MatTableDataSource<Product>;
   displayedColumns: string[] = ['name', 'price', 'audit.updatedDate', 'action'];
-  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
-  selection = new SelectionModel<PeriodicElement>(true, []);
   totalItems = 0;
   queryModel: IQueryModel = {
     pageSize: 5,
@@ -45,20 +21,6 @@ export class ProductListComponent implements OnInit, AfterViewInit {
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
-
-  /** Whether the number of selected elements matches the total number of rows. */
-  isAllSelected() {
-    const numSelected = this.selection.selected.length;
-    const numRows = this.dataSource.data.length;
-    return numSelected === numRows;
-  }
-
-  /** Selects all rows if they are not all selected; otherwise clear selection. */
-  masterToggle() {
-    this.isAllSelected()
-      ? this.selection.clear()
-      : this.dataSource.data.forEach(row => this.selection.select(row));
-  }
 
   constructor(private service: StandardService) {
     this.service.init('product', this.queryModel);
