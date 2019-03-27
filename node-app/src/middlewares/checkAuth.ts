589ba72b453f4e041e0fdc0858872f0d0d0dbcd0
Jacob <zhangen69@gmail.com>
2019-03-27 21:13:20 +0800

    Update checkAuth.ts

--- node-app/src/middlewares/checkAuth.ts
+++ node-app/src/middlewares/checkAuth.ts
@@ -3,7 +3,8 @@ import jwt from 'jsonwebtoken';
 const checkAuth = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(' ')[1];
-        req.auth = jwt.verify(token, 'secret this should be longer');
+        const decodedToken = jwt.verify(token, 'secret this should be longer');
+        req.auth = { isAuth: true, user: decodedToken };
         next();
     } catch (error) {
         res.status(401).json({ message: 'Access Denied!' });
